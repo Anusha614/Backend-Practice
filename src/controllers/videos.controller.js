ff5cb72b78303401c18a29de3dce3cf870d83859
Anusha614 <deshpandeanusha01@gmail.com>
2026-08-19 15:00:27 +0530

    added videos controller

--- src/controllers/videos.controller.js
+++ src/controllers/videos.controller.js
@@ -15,6 +15,48 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    if (!title) {
+        throw new ApiError(400, "Title is required")
+    }
+    if (!description) {
+        throw new ApiError(400, "description is required")
+    }
+
+    const videoFileLocalPath = req.files?.video[0]?.path
+    const thumbnailFileLocalPath = req.files?.thumbnail[0]?.path
+
+    if (!videoFileLocalPath) {
+        throw new ApiError(400, "video file path is required")
+    }
+
+    if (!thumbnailFileLocalPath) {
+        throw new ApiError(400, "thumbnail file path is required")
+    }
+
+    const videoFile = await uploadOnCloudinary(videoFileLocalPath)
+    const thumbnailFile = await uploadOnCloudinary(thumbnailFileLocalPath)
+
+    if (!videoFile) {
+        throw new ApiError(400, "Video upload failed")
+    }
+
+    if (!thumbnail) {
+        throw new ApiError(400, "Thumbnail upload failed")
+    }
+
+    const video = await Video.create({
+        title,
+        description,
+        videoFile: videoFile.url,
+        thumbnail: thumbnailFile.url,
+        duration: videoFile.duration || 0,
+        owner: req.user?._id,
+        isPublished: true
+    })
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, "video uploaded successfully!"))
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
@@ -34,7 +76,53 @@ const getVideoById = asyncHandler(async (req, res) => {
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    const { title, description } = req.body
 
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    const video = await Video.findById(videoId)
+
+    if (!video) {
+        throw new ApiError(404, "Video not found")
+    }
+
+   
+    if (video.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(403, "User not authorized")
+    }
+
+  
+    const updateFields = {}
+
+    if (title) updateFields.title = title
+    if (description) updateFields.description = description
+
+  
+    const thumbnailLocalPath = req.file?.path
+
+    if (thumbnailLocalPath) {
+        const uploadedThumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+        
+        if (!uploadedThumbnail?.url) {
+            throw new ApiError(400, "Failed to upload new thumbnail")
+        }
+        
+        updateFields.thumbnail = uploadedThumbnail.url
+    }
+
+    const updatedVideo = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set: updateFields
+        },
+        { new: true }
+    )
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updatedVideo, "Video updated successfully!"))
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
@@ -62,6 +150,39 @@ const deleteVideo = asyncHandler(async (req, res) => {
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
 
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    const video = await Video.findById(videoId)
+
+    if (!video) {
+        throw new ApiError(404, "Video not found")
+    }
+
+    if (video.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(403, "You are unauthorized to modify this video's publish status")
+    }
+
+    const updatedVideo = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set: {
+                isPublished: !video.isPublished 
+            }
+        },
+        { new: true }
+    )
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200, 
+            updatedVideo, 
+            `Video publish status updated to ${updatedVideo.isPublished ? "Published" : "Unpublished"} successfully!`
+        )
+    )
 })
 
 export {
